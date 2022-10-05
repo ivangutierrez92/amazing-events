30ed23f32a2aa20b8ce7a301ba59aa7d7d4a4962
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-10-05 14:52:33 -0400

    Cards added dynamically from data in Home

---
+++ index.js
@@ -0,0 +1,26 @@
+let events = data.events;
+
+let container = document.getElementById("card-container");
+
+for (let event of events) {
+  let div = document.createElement("div");
+  div.className = "col";
+  div.innerHTML = `
+  <div class="card h-100 shadow px-3 pt-3">
+    <img
+      src="${event.image}"
+      alt=""
+      class="card-img-top fit-cover-sm"
+    />
+    <div class="card-body d-flex flex-column justify-content-between">
+      <h4 class="card-title text-center">${event.name}</h4>
+      <p class="card-text text-center text-truncate">${event.description}</p>
+      <div class="d-flex justify-content-between flex-wrap">
+        <p class="fw-bold my-auto">Price: $${event.price}</p>
+        <button class="btn-pink">See more</button>
+      </div>
+    </div>
+  </div>
+  `;
+  container.appendChild(div);
+}

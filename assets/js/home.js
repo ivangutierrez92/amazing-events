add1e636c4271ab6094cc9b81e3c227ddd9c501f
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-10-11 14:44:42 -0400

    refactorizing code and adding sort

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,26 +1,28 @@
 let events = data.events;
 let container = document.getElementById("card-container");
+let sortedEvents = events.sort((a, b) => new Date(a.date) - new Date(b.date));
+addCardsToContainer(sortedEvents, container);
 
-events.forEach(putCardInContainer);
-
-function putCardInContainer(event) {
-  container.innerHTML += `
-  <div class="col">
-    <div class="card h-100 shadow px-3 pt-3">
-      <img
-        src="${event.image}"
-        alt="${event.name}"
-        class="card-img-top fit-cover-sm"
-      />
-      <div class="card-body d-flex flex-column justify-content-between">
-        <h4 class="card-title text-center">${event.name}</h4>
-        <p class="card-text text-center text-truncate">${event.description}</p>
-        <div class="d-flex justify-content-between flex-wrap">
-          <p class="fw-bold my-auto">Price: $${event.price}</p>
-          <button class="btn-pink">See more</button>
+function addCardsToContainer(events, container) {
+  events.forEach((event) => {
+    container.innerHTML += `
+    <div class="col">
+      <div class="card h-100 shadow px-3 pt-3">
+        <img
+          src="${event.image}"
+          alt="${event.name}"
+          class="card-img-top fit-cover-sm"
+        />
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h4 class="card-title text-center">${event.name}</h4>
+          <p class="card-text text-center text-truncate">${event.description}</p>
+          <div class="d-flex justify-content-between flex-wrap">
+            <p class="fw-bold my-auto">Price: $${event.price}</p>
+            <button class="btn-pink">See more</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   `;
+  });
 }
--- assets/js/upcoming-events.js
+++ assets/js/upcoming-events.js
@@ -2,28 +2,32 @@ let events = data.events;
 let currentDate = new Date(data.currentDate);
 let container = document.getElementById("card-container");
 
-const filteredEvents = events.filter((event) => new Date(event.date) >= currentDate);
-filteredEvents.forEach(putCardInContainer);
+const filteredEvents = events
+  .filter((event) => new Date(event.date) >= currentDate)
+  .sort((a, b) => new Date(a.date) - new Date(b.date));
 
+addCardsToContainer(filteredEvents, container);
 
-function putCardInContainer(event) {
-  container.innerHTML += `
-  <div class="col">
-    <div class="card h-100 shadow px-3 pt-3">
-      <img
-        src="${event.image}"
-        alt="${event.name}"
-        class="card-img-top fit-cover-sm"
-      />
-      <div class="card-body d-flex flex-column justify-content-between">
-        <h4 class="card-title text-center">${event.name}</h4>
-        <p class="card-text text-center text-truncate">${event.description}</p>
-        <div class="d-flex justify-content-between flex-wrap">
-          <p class="fw-bold my-auto">Price: $${event.price}</p>
-          <button class="btn-pink">See more</button>
+function addCardsToContainer(events, container) {
+  events.forEach((event) => {
+    container.innerHTML += `
+    <div class="col">
+      <div class="card h-100 shadow px-3 pt-3">
+        <img
+          src="${event.image}"
+          alt="${event.name}"
+          class="card-img-top fit-cover-sm"
+        />
+        <div class="card-body d-flex flex-column justify-content-between">
+          <h4 class="card-title text-center">${event.name}</h4>
+          <p class="card-text text-center text-truncate">${event.description}</p>
+          <div class="d-flex justify-content-between flex-wrap">
+            <p class="fw-bold my-auto">Price: $${event.price}</p>
+            <button class="btn-pink">See more</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   `;
+  });
 }

c6df050abe6f08fa0b1f7a6029d12e70d5208e6e
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-10-13 11:52:51 -0400

    Combined input and checkbox filters

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,59 +1,103 @@
 let events = data.events;
-let container = document.getElementById("card-container");
+let cardsContainer = document.getElementById("card-container");
 let searchInput = document.getElementById("js-search-input");
 let searchButton = document.getElementById("js-search-button");
 let categories = new Set(data.events.map((event) => event.category));
 let checkboxContainer = document.getElementById("js-checkbox-container");
+let state = {};
 
-searchButton.addEventListener("click", () => {
-  let value = searchInput.value;
-  let filteredEvents = events.filter((event) =>
-    event.name.toLowerCase().includes(value.toLowerCase())
-  );
-  addCardsToContainer(filteredEvents, container);
-});
-
-addCardsToContainer(events, container);
-addCategoriesToContainer(categories, checkboxContainer);
-
-function addCardsToContainer(events, container) {
-  container.innerHTML = "";
-  events.forEach((event) => {
-    container.innerHTML += `
-    <div class="col">
-      <div class="card h-100 shadow px-3 pt-3">
-        <img
-          src="${event.image}"
-          alt="${event.name}"
-          class="card-img-top fit-cover-sm"
-        />
-        <div class="card-body d-flex flex-column justify-content-between">
-          <h4 class="card-title text-center">${event.name}</h4>
-          <p class="card-text text-center text-truncate">${event.description}</p>
-          <div class="d-flex justify-content-between flex-wrap">
-            <p class="fw-bold my-auto">Price: $${event.price}</p>
-            <button class="btn-pink">See more</button>
-          </div>
+let cardTemplate = (name, image, description, price) => {
+  return `
+  <div class="col">
+    <div class="card h-100 shadow px-3 pt-3">
+      <img
+        src="${image}"
+        alt="${name}"
+        class="card-img-top fit-cover-sm"
+      />
+      <div class="card-body d-flex flex-column justify-content-between">
+        <h4 class="card-title text-center">${name}</h4>
+        <p class="card-text text-center text-truncate">${description}</p>
+        <div class="d-flex justify-content-between flex-wrap">
+          <p class="fw-bold my-auto">Price: $${price}</p>
+          <button class="btn-pink">See more</button>
         </div>
       </div>
     </div>
+  </div>
+`;
+};
+
+let checkboxTemplate = (category) => {
+  return `
+  <div class="form-check form-check-inline">
+    <label class="form-check-label" for="${category}">${category}</label>
+    <input
+      class="form-check-input js-category-checkbox"
+      type="checkbox"
+      value="${category}"
+      id="${category}"
+      name="category"
+    />
+  </div>
   `;
+};
+
+addCardsToContainer(events, cardsContainer, cardTemplate);
+addCategoriesToContainer(categories, checkboxContainer, checkboxTemplate);
+
+searchButton.addEventListener("click", () => {
+  let value = searchInput.value;
+  let key = searchInput.name;
+  filterCards(key, value, "input", cardsContainer);
+});
+
+checkboxList = document.querySelectorAll(".js-category-checkbox");
+
+checkboxList.forEach((checkbox) => {
+  checkbox.addEventListener("change", (event) => {
+    let key = event.target.value;
+    let isChecked = event.target.checked;
+    filterCards(key, isChecked, "checkbox", cardsContainer);
+  });
+});
+
+function addCardsToContainer(events, container, template) {
+  container.innerHTML = "";
+  events.forEach((event) => {
+    container.innerHTML += template(
+      event.name,
+      event.image,
+      event.description,
+      event.price
+    );
   });
 }
 
-function addCategoriesToContainer(categories, container) {
+function addCategoriesToContainer(categories, container, template) {
   categories.forEach((category) => {
-    container.innerHTML += `
-    <div class="form-check form-check-inline">
-      <label class="form-check-label" for="${category}">${category}</label>
-      <input
-        class="form-check-input"
-        type="checkbox"
-        value="${category}"
-        id="${category}"
-        name="${category}"
-      />
-    </div>
-    `;
+    container.innerHTML += template(category);
   });
+}
+
+function filterCards(key, value, type, container) {
+  let newEvents = [...events];
+  state[key] = {
+    value: value,
+    type: type,
+  };
+  for (let key in state) {
+    if (state[key].value) {
+      if (state[key].type === "checkbox") {
+        newEvents = newEvents.filter((event) => event.category === key);
+      }
+
+      if (state[key].type === "input") {
+        newEvents = newEvents.filter((event) =>
+          event.name.toLowerCase().includes(state[key].value.toLowerCase())
+        );
+      }
+    }
+  }
+  addCardsToContainer(newEvents, container, cardTemplate);
 }

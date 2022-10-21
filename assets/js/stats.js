5ac9ea8994401b0b2c072c0ec782c7b9366a63bb
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-10-21 17:34:39 -0400

    Added try catch to stats

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,6 +1,7 @@
 let eventsStatisticsContainer = document.getElementById("js-events-statistics");
 let upcomingEventsStatisticsContainer = document.getElementById("js-upcoming-events-statistics");
 let pastEventsStatisticsContainer = document.getElementById("js-past-events-statistics");
+let mainContainer = document.getElementById("js-stats-main");
 
 async function getEvents(query) {
   events = await fetch(`https://mind-hub.up.railway.app/amazing?${query}`);
@@ -28,8 +29,8 @@ function mapEventStatistics(event) {
 function statisticsByCategory(list) {
   return list.reduce((a, b) => {
     let key = a[b.category];
-    let revenue = b.price * attendance;
     let attendance = b.assistance || b.estimate;
+    let revenue = b.price * attendance;
     let capacity = b.capacity;
 
     if (key) {
@@ -93,12 +94,19 @@ function processCategoriesStatistics(events, container) {
 }
 
 async function startProgram() {
-  let pastEvents = await getEvents("time=past");
-  let upcomingEvents = await getEvents("time=upcoming");
+  try {
+    let pastEvents = await getEvents("time=past");
+    let upcomingEvents = await getEvents("time=upcoming");
 
-  processEventsStatistics(pastEvents, eventsStatisticsContainer);
-  processCategoriesStatistics(upcomingEvents, upcomingEventsStatisticsContainer);
-  processCategoriesStatistics(pastEvents, pastEventsStatisticsContainer);
+    processEventsStatistics(pastEvents, eventsStatisticsContainer);
+    processCategoriesStatistics(upcomingEvents, upcomingEventsStatisticsContainer);
+    processCategoriesStatistics(pastEvents, pastEventsStatisticsContainer);
+  } catch(error) {
+    console.log(error);
+    mainContainer.innerHTML = `
+    <div class="w-100"><h2 class="text-center">An error ocurred, and couldn't show the stats. Please, try again later.</h2><div>
+    `;
+  }
 }
 
 startProgram();
